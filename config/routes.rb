3310727c6440fa2cd5ea30d16279592247de1ef3
Seth Lee <Sethlee99@gmail.com>
2018-09-28 13:16:58 +0800

    first

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'story/one'
+  get 'story/two'
+  get 'story/three'
+  get 'story/four'
+  get 'story/five'
+  get 'home/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+end
